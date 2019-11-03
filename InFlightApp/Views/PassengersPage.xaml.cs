af455184a2e06af9a69f336148a6345213eab3e6
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-11-03 20:02:14 +0100

    Add columnheader and rowheader to seating grid

--- InFlightApp/Views/PassengersPage.xaml.cs
+++ InFlightApp/Views/PassengersPage.xaml.cs
@@ -32,28 +32,76 @@ namespace InFlightApp.Views
         {
             string[] rows = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
             int seatsPerRow = 5;
-            for (int i = 0; i < rows.Length; i++)
+
+
+            for (int i = 0; i < rows.Length + 1; i++)
             {
                 RowDefinition rdRow = new RowDefinition();
                 rdRow.Height = new GridLength(1, GridUnitType.Auto);
                 SeatingsGrid.RowDefinitions.Add(rdRow);
 
-                for (int j = 0; j < seatsPerRow; j++)
+                for (int j = 0; j < seatsPerRow + 1; j++)
                 {
-                    ColumnDefinition cdColumn = new ColumnDefinition();
-                    cdColumn.Width = new GridLength(1, GridUnitType.Auto);
-                    SeatingsGrid.ColumnDefinitions.Add(cdColumn);
+                    if (i == 0 && j == 0)
+                    {
+                        ColumnDefinition cdZeroZero = new ColumnDefinition();
+                        cdZeroZero.Width = new GridLength(1, GridUnitType.Auto);
+                        SeatingsGrid.ColumnDefinitions.Add(cdZeroZero);
+                    }
+                    else if (i == 0 && j > 0)
+                    {
+                        ColumnDefinition cdColumnHeader = new ColumnDefinition();
+                        cdColumnHeader.Width = new GridLength(1, GridUnitType.Auto);
+                        SeatingsGrid.ColumnDefinitions.Add(cdColumnHeader);
 
-                    TextBlock tbRow = new TextBlock();
-                    tbRow.Text = $"{rows[i]} - {j + 1}";
-                    tbRow.HorizontalAlignment = HorizontalAlignment.Center;
-                    tbRow.VerticalAlignment = VerticalAlignment.Center;
-                    tbRow.FontSize = 30;
-                    tbRow.Margin = new Thickness(15);
+                        TextBlock tbHeader = new TextBlock();
+                        tbHeader.Text = $"{j}";
+                        tbHeader.HorizontalAlignment = HorizontalAlignment.Center;
+                        tbHeader.VerticalAlignment = VerticalAlignment.Center;
+                        tbHeader.FontSize = 30;
+                        tbHeader.Margin = new Thickness(15);
 
-                    SeatingsGrid.Children.Add(tbRow);
-                    Grid.SetRow(tbRow, i);
-                    Grid.SetColumn(tbRow, j);
+                        SeatingsGrid.Children.Add(tbHeader);
+                        Grid.SetRow(tbHeader, i);
+                        Grid.SetColumn(tbHeader, j);
+
+                    }
+                    else if (i > 0 && j == 0)
+                    {
+
+                        RowDefinition rdHeader = new RowDefinition();
+                        rdHeader.Height = new GridLength(1, GridUnitType.Auto);
+                        SeatingsGrid.RowDefinitions.Add(rdHeader);
+
+                        TextBlock tbHeader = new TextBlock();
+                        tbHeader.Text = $"{rows[i - 1]}";
+                        tbHeader.HorizontalAlignment = HorizontalAlignment.Center;
+                        tbHeader.VerticalAlignment = VerticalAlignment.Center;
+                        tbHeader.FontSize = 30;
+                        tbHeader.Margin = new Thickness(15);
+
+                        SeatingsGrid.Children.Add(tbHeader);
+                        Grid.SetRow(tbHeader, i);
+                        Grid.SetColumn(tbHeader, j);
+
+                    }
+                    else
+                    {
+                        ColumnDefinition cdColumn = new ColumnDefinition();
+                        cdColumn.Width = new GridLength(1, GridUnitType.Auto);
+                        SeatingsGrid.ColumnDefinitions.Add(cdColumn);
+
+                        TextBlock tbName = new TextBlock();
+                        tbName.Text = "Tybo Vanderstraeten";
+                        tbName.HorizontalAlignment = HorizontalAlignment.Center;
+                        tbName.VerticalAlignment = VerticalAlignment.Center;
+                        tbName.FontSize = 30;
+                        tbName.Margin = new Thickness(15);
+
+                        SeatingsGrid.Children.Add(tbName);
+                        Grid.SetRow(tbName, i);
+                        Grid.SetColumn(tbName, j);
+                    }
                 }
             }
         }
