50c35aa03113562332a17de44a1b055cea3c7590
michiel-schoofs <michiel.schoofs@student.hogent.be>
2019-11-10 15:31:11 +0100

    [feat] continuation of product

--- InFlightApp/Model/Product.cs
+++ InFlightApp/Model/Product.cs
@@ -6,6 +6,15 @@ using System.Threading.Tasks;
 namespace InFlightApp.Model
 {
     public class Product{
+        public string ImagePath {
+            get {
+                if (ImageFile == null)
+                    return "/Assets/img/products/default.jpg";
+
+                return ImageFile;
+            }
+        }
+
         public int ProductID { get; set; }
         public decimal UnitPrice { get; set; }
         public int Amount{ get; set; }
