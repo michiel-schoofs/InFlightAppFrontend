416510a166d9412df06bb2044c747384f6e4dbaa
Tybo Vanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-11-19 18:48:11 +0100

    Add notifications page

---
+++ InFlightApp/Views/NotificationsPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace InFlightApp.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class NotificationsPage : Page
+    {
+        public NotificationsPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BtnNotification_Click(object sender, RoutedEventArgs e)
+        {
+            ContentDialog contentDialog = new ContentDialog();
+            contentDialog.Title = "Notification from the crew";
+            contentDialog.CloseButtonText = "Close";
+            contentDialog.DefaultButton = ContentDialogButton.Close;
+            contentDialog.Content = txtNotification.Text;
+            contentDialog.ShowAsync();
+        }
+    }
+}
--- InFlightApp/Views/PassengersPage.xaml.cs
+++ InFlightApp/Views/PassengersPage.xaml.cs
@@ -19,9 +19,6 @@ using Windows.UI.Xaml.Navigation;
 
 namespace InFlightApp.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class PassengersPage : Page
     {
         private PassengersViewModel _model;
