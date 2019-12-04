0af7eca9f63b5a63fb37edd8dcb6a6977874c54c
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-12-04 14:11:43 +0100

    Add custom musicplayer

---
+++ InFlightApp/Converters/DurationConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace InFlightApp.Converters
+{
+    public class DurationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var seconds = (int)Math.Round((double)value);
+            var ticks = seconds * 10000 * 1000;
+            DateTime dt = new DateTime(ticks);
+            return dt.ToString("mm:ss");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- InFlightApp/Views/MusicPage.xaml.cs
+++ InFlightApp/Views/MusicPage.xaml.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -40,19 +42,48 @@ namespace InFlightApp.Views
         private void Song_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
         {
             var song = Songlist.SelectedItem as Music;
-
+            SongSlider.Value = MusicPlayer.Position.TotalSeconds;
             if (!song.IsPlaying)
             {
                 _model.SetPlaying(song);
                 MusicPlayer.Position = TimeSpan.FromSeconds(0);
+                SongSlider.Maximum = MusicPlayer.NaturalDuration.TimeSpan.TotalSeconds;
+                SongSlider.Value = 0;
                 MusicPlayer.Play();
+                SongSlider.Tapped += Slider_Dragged;
+                void Slider_Dragged(object sender2, RoutedEventArgs e2)
+                {
+                    MusicPlayer.Position = TimeSpan.FromSeconds((sender2 as Slider).Value);
+                    ExecuteSlider(SongSlider);
+                }
+                ExecuteSlider(SongSlider);
             }
             else
             {
                 _model.SetNotPlaying();
                 MusicPlayer.Position = TimeSpan.FromSeconds(0);
+                SongSlider.Value = 0;
                 MusicPlayer.Stop();
             }
         }
+
+        private void ExecuteSlider(Slider slider)
+        {
+            var max = slider.Maximum;
+            var pos = slider.Value;
+            Task.Run(async () =>
+             {
+                 while (pos < max)
+                 {
+                     await Task.Delay(1000);
+                     await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                     {
+                         SongSlider.Value += 1;
+                     });
+                     pos += 1;
+                 }
+             });
+        }
+
     }
 }
