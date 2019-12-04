4cac7138ffca91c1b1f6c2d4e67e06685895f1f7
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-12-04 10:27:22 +0100

    Add infoViewModel

---
+++ InFlightApp/View Model/InfoViewModel.cs
@@ -0,0 +1,43 @@
+﻿using InFlightApp.Configuration;
+using InFlightApp.Model;
+using InFlightApp.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InFlightApp.View_Model
+{
+    public class InfoViewModel
+    {
+        private readonly IFlightService _flightService;
+
+        public Flight Flight { get; set; }
+        public Weather Weather { get; set; }
+
+        public InfoViewModel()
+        {
+            try
+            {
+                _flightService = ServiceLocator.Current.GetService<IFlightService>(true);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public void LoadFlightInfo()
+        {
+
+            Flight = _flightService.GetFlight();
+
+        }
+
+        public void LoadWeatherInfo()
+        {
+            Weather = _flightService.GetCurrentWeather(Flight.Destination.City, Flight.Destination.Country);
+        }
+    }
+}
