d82bc6a1d8840be1f2b0de1c7f10614879f45ce0
LarsVandenberghe <lars.vandenberghe@student.hogent.be>
2019-12-11 20:36:56 +0100

    [feat] Vertalingen

---
+++ InFlightApp/Converters/ProductTypeConverter.cs
@@ -0,0 +1,28 @@
+﻿using InFlightApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace InFlightApp.Converters {
+    public class ProductTypeConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, string language) {
+            try {
+                
+                ProductType item = Enum.Parse<ProductType>((string)value);
+                var resourceBundle = Windows.ApplicationModel.Resources.ResourceLoader.GetForCurrentView();
+
+                return resourceBundle.GetString(item.ToString());
+            } catch (Exception e) {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) {
+            throw new NotImplementedException();
+        }
+    }
+}
